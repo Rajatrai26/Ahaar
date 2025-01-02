5f7c446b86143a675d71aaa00cb3d85a00bbc8ee
Rajatrai26 <rajatrai2606@gmail.com>
2025-01-02 13:39:33 +0530

    add button 4 28 50

--- lib/admin/add_food.dart
+++ lib/admin/add_food.dart
@@ -204,11 +204,7 @@ class _AddFoodState extends State<AddFood> {
                     )),
                   ),
                 ),
-<<<<<<< HEAD
               ) //add button
-=======
-              )
->>>>>>> 8dbbd2c62ce7961b8398413b2f360602ed00a48f
             ],
           ),
         ),
